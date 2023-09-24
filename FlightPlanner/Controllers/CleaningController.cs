d799e6836418cffd53edd4f7be1cb09b21a36d36
Peteris Krumins <128175043+Spechka@users.noreply.github.com>
2023-09-24 18:03:29 +0300

    Add files via upload

---
+++ FlightPlanner/Controllers/CleaningController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("testing-api")]
+    [ApiController]
+    public class CleaningController : ControllerBase
+    {
+        [Route("clear")]
+        [HttpPost]
+        public IActionResult Clear()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ FlightPlanner/Models/Airport.cs
@@ -0,0 +1,6 @@
+﻿namespace FlightPlanner.Models
+{
+    public class Airport
+    {
+    }
+}
